520d857efb7292340f744e0bb9caa82d100af662
shangopi <gopinath.19@cse.mrt.ac.lk>
2024-04-27 23:04:35 +0530

    corrections

---
+++ navigation/GetLocationPrediction.js
@@ -0,0 +1,21 @@
+const KNN = require('ml-knn');
+const {test_labels, test_data} = require('./data');
+
+async function getLocationPrediction(currentWifiStrengths) {
+  try {
+    const sizel = test_data.length;
+    
+    const train_data_set = test_data;
+
+    var knn = new KNN(train_data_set, test_labels, {k: 3}); // consider 2 nearest neighbors
+
+    var ans = knn.predict(currentWifiStrengths);
+    console.log('ans =', ans);
+    return ans;
+  } catch (e) {
+    console.log('Error', e);
+  }
+}
+
+getLocationPrediction();
+module.exports = {getLocationPrediction};
--- screens/MainMap.tsx
+++ screens/MainMap.tsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect,useMemo} from 'react';
 import WifiReborn from 'react-native-wifi-reborn';
-import {getLocationPrediction} from '../navigation/test'
+import {getLocationPrediction} from '../navigation/GetLocationPrediction'
 import {graphJsonString} from '../navigation/constants';
 import {getWifiStrengths} from '../navigation/wifiRefinedInputUtil'
 
@@ -26,19 +26,14 @@ const MainMap = () => {
       return [horizontalLength, verticalLength];
    };
 
-   // Call the function
-   const [horizontalLength, verticalLength] = calculateGridNumber(568);
+   const [gridID, setgridID] = useState(87);
+   const [horizontalLength, verticalLength] = calculateGridNumber(gridID);
    const topOutterMap = 42;
    const mapTopMargin = 31;
    const mapLeftMargin = 20;
    
-   const [markerPosition, setMarkerPosition] = useState({ x: mapLeftMargin + verticalLength + 8, y: topOutterMap + mapTopMargin + horizontalLength + 8 });
+   let markerPosition = [mapLeftMargin + verticalLength + 8, topOutterMap + mapTopMargin + horizontalLength + 8] ;
    const [zoomLevel, setZoomLevel] = useState(1);
-
-   const graphJsonInput = useMemo(() => {
-      setMarkerPosition({x: windowWidth / 2, y: windowHeight / 2});
-      return JSON.parse(graphJsonString);
-    }, []);
 
    useEffect(() => {
       const getPredictedGrid = async () => {
@@ -57,47 +52,20 @@ const MainMap = () => {
   
         const currentWifiStrengths = getWifiStrengths(wifiStringName);
         const gidId = await getLocationPrediction(currentWifiStrengths);
+        setgridID(gidId);
         wifiStringName = [];
-        try {
-          const nodeData = graphJsonInput?.nodes?.find(node => {
-            return node.id === gidId;
-          })?.data;
-          setMarkerPosition({
-            x: (nodeData.x + 1) * 50,
-            y: (nodeData.y + 1) * 50,
-          });
-        } catch (e) {
-          console.log(e);
-        }
+        
       };
   
       getPredictedGrid();
   
       const interval = setInterval(() => {
         getPredictedGrid();
-      }, 5000);
+      }, 15000);
   
       return () => clearInterval(interval);
     }, []);
 
-   const panResponder = PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onPanResponderMove: (event, gesture) => {
-         console.log('Gesture:', gesture);
-         // Handle panning
-         setMarkerPosition({
-            x: markerPosition.x + gesture.dx,
-            y: markerPosition.y + gesture.dy,
-         });
-
-         // Handle pinch gesture for zooming
-         if (gesture.state === PanResponder.STATE_CHANGED && gesture.pinch) {
-            const newZoomLevel = zoomLevel * gesture.pinch;
-            // Enforce zoom limits (optional)
-            setZoomLevel(Math.max(1, Math.min(3, newZoomLevel))); // Limit zoom between 1x and 3x
-         }
-      },
-   });
 
    return (
       <View style={styles.container}>
@@ -110,9 +78,8 @@ const MainMap = () => {
          <View
             style={[
                styles.marker,
-               { left: markerPosition.x, top: markerPosition.y },
+               { left: markerPosition[0], top: markerPosition[1] },
             ]}
-            {...panResponder.panHandlers}
          />
       </View>
    );
