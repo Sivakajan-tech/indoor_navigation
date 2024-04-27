257a182bd5b4e5e4b416fdcac4f4fe9643b887e6
shangopi <gopinath.19@cse.mrt.ac.lk>
2024-04-27 18:57:25 +0530

    adding map screen

--- screens/MainMap.tsx
+++ screens/MainMap.tsx
@@ -1,9 +1,63 @@
-import React, {useState} from 'react';
-import {View, Image, StyleSheet, PanResponder} from 'react-native';
+import React, {useState, useEffect,useMemo} from 'react';
+import WifiReborn from 'react-native-wifi-reborn';
+import {getLocationPrediction} from '../navigation/test'
+import {graphJsonString} from '../navigation/constants';
+import {getWifiStrengths} from '../navigation/wifiRefinedInputUtil'
+
+import {View, Image, StyleSheet, PanResponder,Dimensions} from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height - 64;
+
 
 const MainMap = () => {
    const [markerPosition, setMarkerPosition] = useState({x: 0, y: 0});
    const [zoomLevel, setZoomLevel] = useState(1);
+
+   const graphJsonInput = useMemo(() => {
+      setMarkerPosition({x: windowWidth / 2, y: windowHeight / 2});
+      return JSON.parse(graphJsonString);
+    }, []);
+
+   useEffect(() => {
+      const getPredictedGrid = async () => {
+        let wifiStringName = [];
+        const wifiList = await WifiReborn?.loadWifiList();
+  
+        wifiList?.map(wifi => {
+          let wifiData = '';
+          wifiData += wifi.BSSID;
+          wifiData += '=';
+          wifiData += wifi.SSID;
+          wifiData += '=';
+          wifiData += wifi.level;
+          wifiStringName.push(wifiData);
+        });
+  
+        const currentWifiStrengths = getWifiStrengths(wifiStringName);
+        const gidId = await getLocationPrediction(currentWifiStrengths);
+        wifiStringName = [];
+        try {
+          const nodeData = graphJsonInput?.nodes?.find(node => {
+            return node.id === gidId;
+          })?.data;
+          setMarkerPosition({
+            x: (nodeData.x + 1) * 50,
+            y: (nodeData.y + 1) * 50,
+          });
+        } catch (e) {
+          console.log(e);
+        }
+      };
+  
+      getPredictedGrid();
+  
+      const interval = setInterval(() => {
+        getPredictedGrid();
+      }, 5000);
+  
+      return () => clearInterval(interval);
+    }, []);
 
    const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
