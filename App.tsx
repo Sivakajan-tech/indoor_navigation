119b8211a8eaae93d8ae77c65bae013c65f33fb5
sivakajan-tech <my.otherwrks@gmail.com>
2024-04-28 20:38:54 +0530

    Add mainmap

--- App.tsx
+++ App.tsx
@@ -1,40 +1,41 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import Welcome from './screens/Welcome';
 import MainMap from './screens/MainMap';
 
 const Stack = createStackNavigator();
 const App = () => {
-   const linking = {
-      prefixes: ['pocketpath://'],
-      config: {
-         screens: {
-            Welcome: 'welcome',
-         },
-      },
-   };
-   return (
-      <NavigationContainer linking={linking}>
-         <Stack.Navigator initialRouteName="Welcome">
-            <Stack.Screen
-               name="Welcome"
-               component={Welcome}
-               options={{
-                  headerShown: false,
-               }}
-            />
-            <Stack.Screen
-               name="MainMap"
-               component={MainMap}
-               options={{
-                  headerShown: false,
-               }}
-            />
-         </Stack.Navigator>
-      </NavigationContainer>
-   );
+  const linking = {
+    prefixes: ['pocketpath://'],
+    config: {
+      screens: {
+        Welcome: 'welcome',
+        MainMap:  'mainmap/:gridID',
+      }
+    },
+  };
+  return (
+    <NavigationContainer linking={linking}>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="MainMap"
+          component={MainMap}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
